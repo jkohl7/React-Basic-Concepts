4c03958c3edf326bc679e8968317ac4c8a1c1477
Jacob Kohl <jkohl7@my.wctc.edu>
2022-01-27 15:15:43 -0600

    Silver and Bronze Added

--- src/App.js
+++ src/App.js
@@ -5,20 +5,55 @@ import './App.css';
 class App extends Component {
   state = {
     countries: [
-      { id: 1, name: 'United States', gold: 2 },
-      { id: 2, name: 'China', gold: 3 },
-      { id: 3, name: 'Germany', gold: 0 },
+      { id: 1, name: 'United States', gold: 3, silver: 2, bronze: 1 },
+      { id: 2, name: 'China', gold: 1, silver: 2, bronze: 3 },
+      { id: 3, name: 'Germany', gold: 2, silver: 3, bronze: 4 },
+    ],
+    medals: [
+      { id: 1, name: 'gold' },
+      { id: 2, name: 'silver' },
+      { id: 3, name: 'bronze' },
     ]
-    }
+  }
+  handleIncrement = (countryId, medalName) => {
+    const countries = [ ...this.state.countries ];
+    const idx = countries.findIndex(c => c.id === countryId);
+    countries[idx][medalName] += 1;
+    this.setState({ countries: countries });
+  }
+  handleDecrement = (countryId, medalName) => {
+    const countries = [ ...this.state.countries ];
+    const idx = countries.findIndex(c => c.id === countryId);
+    countries[idx][medalName] -= 1;
+    this.setState({ countries: countries });
+  }
+  getAllMedalsTotal() {
+    let sum = 0;
+    this.state.medals.forEach(medal => { sum += this.state.countries.reduce((a, b) => a + b[medal.name], 0); });
+    return sum;
+  }
   render() { 
     return (
       <React.Fragment>
-       {this.state.countries.map(country =>
-        <Country key={country.id} country={country} />)}
+        <div className='appHeading'>
+          Olympic Medals 
+          <span className='badge'>
+            { this.getAllMedalsTotal() }
+          </span>
+        </div>
+        <div className='countries'>
+            { this.state.countries.map(country => 
+              <Country 
+                key={ country.id } 
+                country={ country } 
+                medals={ this.state.medals }
+                onIncrement={ this.handleIncrement } 
+                onDecrement={ this.handleDecrement } />
+            )}
+        </div>
       </React.Fragment>
     );
   }
 }
-
  
 export default App;
