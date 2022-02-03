d38e80d63de42273e17f8ac9eab5aa17f951c056
Jacob Kohl <jkohl7@my.wctc.edu>
2022-02-03 17:16:08 -0600

    Revert "Add Word"
    This reverts commit a23abdd1d5d77400c9a443b6576549ec0e6877fa.

--- src/components/Counter.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Counter extends Component {
-    render() { 
-        return ( <div className='counter'>words: { this.props.totalWords }</div> );
-    }
-}
-
-export default Counter;
