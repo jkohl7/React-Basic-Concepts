a23abdd1d5d77400c9a443b6576549ec0e6877fa
Jacob Kohl <jkohl7@my.wctc.edu>
2022-02-03 16:36:09 -0600

    Add Word

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Word from './components/Word';
+import Counter from './components/Counter';
+import NewWord from './components/NewWord';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    words: [
+      { id: 1, word: 'banana', color: 'yellow' },
+      { id: 2, word: 'apple', color: 'red' },
+      { id: 3, word: 'lime', color: 'green' },
+    ]
+  }
+  handleDelete = (wordId) => {
+    const words = this.state.words.filter(w => w.id !== wordId);
+    this.setState({ words:words });
+  }
+  handleAdd = (word, color) => {
+    const { words } = this.state;
+    const id = words.length === 0 ? 1 : Math.max(...words.map(word => word.id)) + 1;
+    const mutableWords = words.concat({ id: id, word: word, color: color });
+    this.setState({ words:mutableWords });
+  }
+  componentDidMount() {
+    console.log("App mounted");
+  }
+  render() { 
+    const { words } = this.state;
+    return ( 
+      <div className="App">
+        <header className="App-header">
+          nugatory
+        </header>
+        { words.map(word => 
+          <Word 
+            key={ word.id } 
+            word={ word } 
+            onDelete={ this.handleDelete } />
+        )}
+        <Counter totalWords={ words.length } />
+        <NewWord onAdd={ this.handleAdd } />
+      </div>
+     );
+  }
 }
-
+ 
 export default App;
