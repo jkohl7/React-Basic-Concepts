7ff7a39823972f263f2a62c5a32ab21fd79933fd
Jacob Kohl <jkohl7@my.wctc.edu>
2022-02-07 14:43:08 -0600

    add and delete

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import Country from './components/Country';
+import NewCountry from './components/NewCountry';
 import './App.css';
 
 class App extends Component {
   state = {
     countries: [
-      { id: 1, name: 'United States', gold: 3, silver: 2, bronze: 1 },
-      { id: 2, name: 'China', gold: 1, silver: 2, bronze: 3 },
-      { id: 3, name: 'Germany', gold: 2, silver: 3, bronze: 4 },
+      { id: 1, name: 'United States', gold: 2, silver: 2, bronze: 3 },
+      { id: 2, name: 'China', gold: 3, silver: 1, bronze: 0 },
+      { id: 3, name: 'Germany', gold: 0, silver: 2, bronze: 2 },
     ],
     medals: [
       { id: 1, name: 'gold' },
       { id: 2, name: 'silver' },
       { id: 3, name: 'bronze' },
     ]
+  }
+  handleAdd = (name) => {
+    const { countries } = this.state;
+    const id = countries.length === 0 ? 1 : Math.max(...countries.map(country => country.id)) + 1;
+    const mutableCountries = [...countries].concat({ id: id, name: name, gold: 0, silver: 0, bronze: 0 });
+    this.setState({ countries: mutableCountries });
+  }
+  handleDelete = (countryId) => {
+    const { countries } = this.state;
+    const mutableCountries = [...countries].filter(c => c.id !== countryId);
+    this.setState({ countries: mutableCountries });
   }
   handleIncrement = (countryId, medalName) => {
     const countries = [ ...this.state.countries ];
@@ -36,7 +48,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <div className='appHeading'>
-          Olympic Medals 
+          Olympic Medals
           <span className='badge'>
             { this.getAllMedalsTotal() }
           </span>
@@ -47,10 +59,12 @@ class App extends Component {
                 key={ country.id } 
                 country={ country } 
                 medals={ this.state.medals }
+                onDelete={ this.handleDelete }
                 onIncrement={ this.handleIncrement } 
                 onDecrement={ this.handleDecrement } />
             )}
         </div>
+        <NewCountry onAdd={ this.handleAdd } />
       </React.Fragment>
     );
   }
